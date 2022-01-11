fcec4d7b1ea0c64b157197252b737aa5f50e4573
healyr4 <healyr4@tcd.ie>
2022-01-11 13:34:58 +0000

    Add files via upload

--- Part1.java
+++ Part1.java
@@ -5,51 +5,155 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+import edu.duke.*;
+import java.io.File;
 public class Part1 {
+    public int findStopCodon(String dna, int startIndex, String stopCodon){
+       int currIndex = 0;
+       int diff = 0;
+       currIndex = dna.indexOf(stopCodon,startIndex+3);
+       while (currIndex != -1){
+           diff = currIndex - startIndex;
+           if (diff%3==0){
+               return currIndex;
+           }
+           else {
+               currIndex = dna.indexOf(stopCodon, currIndex+1);
+           }
+       }
+        
+       return dna.length(); 
+    }
     
-    public static String findSimpleGene(String dna){
+    public void testFindStopCodon(){
+        int test1 = findStopCodon("AGTACTTAATAG",0, "TAG");
+        System.out.println("Test index is: " + test1);
+        int test2 = findStopCodon("ACTAGTACTTAATAG",0, "TGA");
+        System.out.println("Test index is: " + test2);  
+        int test3 = findStopCodon("AGTGATTAATAG",0, "TGA");
+        System.out.println("Test index is: " + test3);   
+        
+        String test4 = findGene("ACTATGATTATATAA");
+        System.out.println("Gene is: " + test4);  
+    }
+    
+    public String findGene(String dna){
         int start_index = dna.indexOf("ATG");
-        System.out.println("Start index: " + start_index);
-        if (start_index == -1){
+        if(start_index == -1){
+            return ""; 
+        }
+        
+        int taaIndex = findStopCodon(dna, start_index, "TAA");
+        int tagIndex = findStopCodon(dna, start_index, "TAG");
+        int tgaIndex = findStopCodon(dna, start_index, "TGA");
+        //System.out.println("TAA : " + taaIndex);
+        //System.out.println("TAG : " + tagIndex);
+        //System.out.println("TGA : " + tgaIndex);
+        int minIndex = -1;
+        if( taaIndex > -1 && taaIndex < tagIndex){
+            minIndex = taaIndex;
+        }
+        else{
+            minIndex = tagIndex;
+        }
+            
+        if (tgaIndex > -1 && tgaIndex < minIndex){
+            minIndex = tgaIndex;
+        }
+        
+        if (minIndex == -1 || minIndex >= dna.length()-1){
             return "";
         }
         
-        int end_index = dna.indexOf("TAA",start_index);
-        if(end_index == -1){
+        else{
+            return dna.substring(start_index,minIndex +3);
+        }
+        
+    
+    }
+    //Used method overloading 
+    public String findGene(String dna, int startIndex){
+        int start_index = dna.indexOf("ATG",startIndex);
+        if(start_index == -1){
+            return ""; 
+        }
+        
+        int taaIndex = findStopCodon(dna, start_index, "TAA");
+        int tagIndex = findStopCodon(dna, start_index, "TAG");
+        int tgaIndex = findStopCodon(dna, start_index, "TGA");
+        //System.out.println("TAA : " + taaIndex);
+        //System.out.println("TAG : " + tagIndex);
+        //System.out.println("TGA : " + tgaIndex);
+        int minIndex = -1;
+        if( taaIndex > -1 && taaIndex < tagIndex){
+            minIndex = taaIndex;
+        }
+        else{
+            minIndex = tagIndex;
+        }
+            
+        if (tgaIndex > -1 && tgaIndex < minIndex){
+            minIndex = tgaIndex;
+        }
+        
+        if (minIndex == -1 || minIndex >= dna.length()-1){
             return "";
         }
-        System.out.println("End index: " + end_index);
         
-        int diff = start_index - end_index;
-        if (diff%3==0){
-            return dna.substring(start_index,end_index+3);
+        else{
+            return dna.substring(start_index,minIndex +3);
         }
         
-        return "";
+    
     }
-       
-    public static void testSimpleGene(){
-        String string1 = "ATGAGAAAGTAA";
-        System.out.println("String 1 is: " + string1);
-        String ans1 = findSimpleGene(string1);
-        System.out.println("String 1 is: " + string1 +" Answer is"+ans1);
-        
-        String string2 = "AAGAGAATGTAA";
-        String ans2 = findSimpleGene(string2);
-        System.out.println("String 2 is: " + string1 +" Answer is"+ans2);
-        
-        String string3 = "ATGAGAATGTGA";
-        String ans3 = findSimpleGene(string3);
-        System.out.println("String 3 is: " + string3 +" Answer is"+ans3);
-        
-        String string4 = "ATGAGATGTAA";
-        String ans4 = findSimpleGene(string4);
-        System.out.println("String 4 is: " + string4 +" Answer is"+ans4);
-        
-        String string5 = "";
-        String ans5 = findSimpleGene(string5);
-        System.out.println("String 5 is: " + string5 +" Answer is"+ans5);
-   
+    
+    
+    public void printAllGenes(String dna){
+        int startIndex = 0;
+        while (true){
+            String strand = findGene(dna,startIndex);
+            //System.out.println("Strand is: " + strand);
+            if (strand.isEmpty()){
+                System.out.println("Done");
+                break;
+            }
+            System.out.println(strand);
+            
+            startIndex = dna.indexOf(strand,startIndex) + strand.length();
+            //System.out.println("Start index: "+startIndex);
+        }
     }
-
+    
+    public StorageResource getAllGenes(String dna){
+        StorageResource geneList = new StorageResource();
+        int startIndex = 0;
+        while (true){
+            String strand = findGene(dna,startIndex);
+            //System.out.println("Strand is: " + strand);
+            if (strand.isEmpty()){
+                System.out.println("Done");
+                break;
+            }
+            geneList.add(strand);
+            
+            startIndex = dna.indexOf(strand,startIndex) + strand.length();
+            //System.out.println("Start index: "+startIndex);
+        }
+        return geneList;
+    }
+    
+    public void testPrintAllGenes(){
+        System.out.println("STARTING TESTING ");
+        printAllGenes("CTATGCCTTAAATTATGGTTTAG");
+    }
+    
+    public void getAllGenes(){
+        System.out.println("STARTING TESTING ");
+        StorageResource geneList = getAllGenes("CTATGCCTTAAATTATGGTTTAG");
+        for (String gene: geneList.data()){
+            System.out.println("Gene: " + gene);
+        }
+        
+    }
+    
 }
